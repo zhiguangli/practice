7999b19dcffa735b8494656941ebf83e51273952
zhiguangli <252859182@qq.com>
2019-06-04 14:58:12 +0800

    Create calculator.h

---
+++ calculator/calculator.h
@@ -0,0 +1,24 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <string>
+#include <iostream>
+
+class Calculator
+{
+public:
+    Calculator();
+
+    void calculate(const std::string &expr);
+    void output() const { std::cout << expr << " = " << result << std::endl; }
+    std::string toPostExpression(const std::string &expr);
+private:
+
+    std::string calculatePostExpression(const std::string &postExpr);
+    std::string fillZeroToExpression(const std::string &expr);
+private:
+    std::string expr;
+    std::string result;
+};
+
+#endif // CALCULATOR_H
